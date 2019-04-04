4e38d2e29876c0e446e08cebc0067833f11809fd
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2019-04-04 16:19:36 +0300

    Added some stuff...

--- main.py
+++ main.py
@@ -5,29 +5,32 @@ from minesweeper import Minesweeper
 
 
 class GUI():
-    def __init__(self, boardSizeX, boardSizeY):
+    def __init__(self, boardSizeX, boardSizeY, mineCount):
+        self.__game = Minesweeper(boardSizeX, boardSizeY, mineCount)
         self.__window = tk.Tk()
         self.__cCountX = boardSizeX
         self.__cCountY = boardSizeY
-        self.__cellButtons = []
+        self.__cellButtons = {}
+        self.__pressed = [0, 3]
+        self.__l1 = tk.Label(self.__window, text="Mines left: " + str(self.__game.getMines()))
+        self.__l1.grid(row=0, column=0, columnspan=4)
         # Make a board
         self.initializeBoard()
 
     def initializeBoard(self):
-
+        # Really weird shit...
         for i in range(self.__cCountX):
             for j in range(self.__cCountY):
+                btnToAdd = tk.Button(self.__window, text="   ").grid(row=1 + i, column=j)
                 # Set the grid for every button
-                self.__cellButtons.append(tk.Button(self.__window, text="0").grid(row=i, column=j))
-                # self.__cellButtons[i * self.__cCountY + j].grid(row=i, column=j)
+                self.__cellButtons[j * self.__cCountX + i] = btnToAdd
 
     def start(self):
         self.__window.mainloop()
 
 
 def main():
-    game = Minesweeper(9, 9)
-    gui = GUI(9, 9)
+    gui = GUI(9, 9, 10)
     gui.start()
 
 
--- minesweeper.py
+++ minesweeper.py
@@ -5,19 +5,29 @@ from cell import Cell
 
 class Minesweeper():
 
-    def __init__(self, iWidth, iHeight):
-        self.__width = iWidth
-        self.__height = iHeight
+    def __init__(self, width, height, mines):
+        self.__width = width
+        self.__height = height
 
-        self.__board = [[0 for i in range(iWidth)] for j in range(iHeight)]
+        self.__mines = mines
+
+        self.__board = [Cell(0, 0, False)] * (width * height + width)
         self.initializeBoard()
 
     def initializeBoard(self):
         for i in range(self.__width):
             for j in range(self.__height):
-                self.__board[i][j] = Cell(i, j, False)
+                self.__board[j * self.__height + i] = Cell(i, j, False)
+
+    def getMines(self):
+        mineCount = 0
+        for i in range(self.__width):
+            for j in range(self.__height):
+                if self.__board[j * self.__height + i].isMine():
+                    mineCount += 1
+        return mineCount
 
     def printBoard(self):
         for i in range(self.__width):
             for j in range(self.__height):
-                print(self.__board[i][j])
+                print(self.__board[j * self.__height + i])
