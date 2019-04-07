8ef40e11387ddb4adbbb989041c7efc43682fa51
Joonas Kakkuri <joonas.kakkuri@gmail.com>
2019-04-07 20:18:42 +0300

    v2.0

--- main.py
+++ main.py
@@ -6,31 +6,100 @@ from minesweeper import Minesweeper
 
 class GUI():
     def __init__(self, boardSizeX, boardSizeY, mineCount):
-        self.__game = Minesweeper(boardSizeX, boardSizeY, mineCount)
         self.__window = tk.Tk()
-        self.__cCountX = boardSizeX
-        self.__cCountY = boardSizeY
-        self.__cellButtons = {}
-        self.__pressed = [0, 3]
-        self.__l1 = tk.Label(self.__window, text="Mines left: " + str(self.__game.getMines()))
-        self.__l1.grid(row=0, column=0, columnspan=4)
-        # Make a board
-        self.initializeBoard()
+        self.__game = Minesweeper(
+            self.__window, boardSizeX, boardSizeY, mineCount)
 
-    def initializeBoard(self):
-        # Really weird shit...
-        for i in range(self.__cCountX):
-            for j in range(self.__cCountY):
-                btnToAdd = tk.Button(self.__window, text="   ").grid(row=1 + i, column=j)
-                # Set the grid for every button
-                self.__cellButtons[j * self.__cCountX + i] = btnToAdd
+        self.__width = boardSizeX
+        self.__height = boardSizeY
+        self.__l1 = tk.Label(
+            self.__window, text="Mines left: " + str(self.__game.getMineCount()))
+        self.__l1.grid(row=0, column=0)
+
+        # Create buttons
+        self.createButtons()
+
+        # Update game
+        self.__game.update()
 
     def start(self):
         self.__window.mainloop()
 
+    def reset(self):
+        self.__game.hideAll()
+        self.__game.initializeBoard()
+        if self.__flagBtn.cget("bg") == "red":
+            self.flag()
+
+    def createButtons(self):
+        # Flag - Button
+        self.__flagBtn = tk.Button(
+            self.__window, text="Flag OFF", command=self.flag)
+        self.__flagBtn.grid(row=1, column=0)
+        self.__flagBtn.configure(bg='grey')
+
+        # Reset - Button
+        self.__resetBtn = tk.Button(
+            self.__window, text="Reset game", command=self.reset)
+        self.__resetBtn.grid(row=2, column=0)
+
+        # End - Button
+        self.__endBtn = tk.Button(
+            self.__window, text="QUIT", command=self.quitGame)
+        self.__endBtn.grid(row=4, column=0)
+
+    def quitGame(self):
+        self.__window.destroy()
+
+    def flag(self):
+        self.__game.toggleFlag()
+
+        if self.__game.flag():
+            self.__flagBtn.configure(bg='red', text="Flag ON")
+
+        else:
+            self.__flagBtn.configure(bg="grey", text="Flag OFF")
+
+
+def menu():
+    root = tk.Tk()
+
+    tk.Label(root, text="Enter the width:").grid(row=0, column=0)
+    tk.Label(root, text="Enter the height:").grid(row=1, column=0)
+    tk.Label(root, text="Enter the amount of mines:").grid(row=2, column=0)
+
+    width = tk.IntVar(value=None)
+    widthEntry = tk.Entry(root,  textvariable=width)
+    widthEntry.grid(row=0, column=1)
+
+    height = tk.IntVar()
+    heightEntry = tk.Entry(root, textvariable=height)
+    heightEntry.grid(row=1, column=1)
+
+    minecount = tk.IntVar()
+    mineEntry = tk.Entry(root, textvariable=minecount)
+    mineEntry.grid(row=2, column=1)
+
+    startBtn = tk.Button(root, text="Start game",
+                         command=lambda: root.destroy())
+    startBtn.grid(row=3, column=2)
+    root.mainloop()
+
+    return width.get(), height.get(), minecount.get()
+
 
 def main():
-    gui = GUI(9, 9, 10)
+
+    w, h, m = menu()
+
+    if m >= w*h / 10:
+        # Too many mines...
+        pass
+
+    GRIDWIDTH = h
+    GRIDHEIGHT = w
+    MINES = m
+    gui = GUI(GRIDWIDTH, GRIDHEIGHT, MINES)
     gui.start()
 
 
--- minesweeper.py
+++ minesweeper.py
@@ -1,33 +1,189 @@
 
 from cell import Cell
+from random import randint
 # This class holds data about minesweeper field
 
 
 class Minesweeper():
+    """
+        Minesweeper is an object, that hold information about minesweeper game´s
+        state. This is not responsible for displaying, it just holds essential
+        information related to the game.
+    """
 
-    def __init__(self, width, height, mines):
+    def __init__(self, master, width, height, mines):
+        self.__master = master
         self.__width = width
         self.__height = height
 
+        # Indicates wether the flag is on or off
+        self.__flag = False
         self.__mines = mines
 
-        self.__board = [Cell(0, 0, False)] * (width * height + width)
-        self.initializeBoard()
+        # Initialize 2-dimensional data structure
+        self.__board = [[None for i in range(
+            self.__width)] for j in range(self.__height)]
 
-    def initializeBoard(self):
+        self.initializeBoard()  # Initialize and place Cells on correct positions
+
+    def toggleFlag(self):
+        """ Toggle wether player wants to flag cell of turn a cell """
+        self.__flag = False if self.__flag else True
+
+    def flag(self):
+        return self.__flag
+
+    def showAll(self):
         for i in range(self.__width):
             for j in range(self.__height):
-                self.__board[j * self.__height + i] = Cell(i, j, False)
+                self.__board[i][j].show()
+
+    def hideAll(self):
+        for i in range(self.__width):
+            for j in range(self.__height):
+                self.__board[i][j].__isRevealed = False
+
+    def gameOver(self):
+        self.showAll()
+
+    def initializeBoard(self):
+        """ Initialize the board with random minepositions """
+
+        # Initialize random positions for mines
+        minepos = []
+
+        while len(minepos) < self.__mines:
+
+            pos = [randint(0, self.__width-1), randint(0, self.__height-1)]
+            if pos in minepos:
+                continue
+            else:
+                minepos.append(pos)
+
+        # Populate new board with Cells
+        # TODO: Find a way that player doesn't lose on first turn
+        for i in range(self.__width):
+            for j in range(self.__height):
+                if [i, j] in minepos:
+                    state = True
+                else:
+                    state = False
+
+                self.__board[i][j] = Cell(i, j, state, self.__master, self)
+
+        # Get random Cells and convert them to mines.
+
+        for i in range(self.__width):
+            for j in range(self.__height):
+                self.countNeighbours(i, j)
 
     def getMines(self):
+        return [j for sub in self.__board for j in sub]
+
+    def getMineCount(self):
+        """
+        Returns how many mines are not revealed.
+        """
         mineCount = 0
         for i in range(self.__width):
             for j in range(self.__height):
-                if self.__board[j * self.__height + i].isMine():
+                if self.__board[i][j].isMine():
                     mineCount += 1
         return mineCount
 
-    def printBoard(self):
+    def update(self):
+        """ Update board """
         for i in range(self.__width):
             for j in range(self.__height):
-                print(self.__board[j * self.__height + i])
+                # Let's update this cell
+                cell = self.__board[i][j]
+
+                if cell.isRevealed():
+                    if cell.getCellNumber() == 0:
+                        # No mines nearby, so lets update surrounding neighbours
+                        self.updateNeighbours(i, j)
+                    else:
+                        # cell has mine in its neighbour
+                        cell.update()
+                else:
+                    cell.update()
+
+    def updateNeighbours(self, cellX, cellY):
+        """ Update neighbour cells around cellX and cellY
+            :param cellX, cellY: Cell coordinates, which neighbours should be
+                                 updated
+            :return None:
+        """
+        # Get offset around current cell
+        for x in range(-1, 2):
+            for y in range(-1, 2):
+
+                if x == 0 and y == 0:
+                    # Update current cell
+                    self.__board[cellX][cellY].update()
+                    continue
+
+                i = x + cellX
+                j = y + cellY
+
+                if i < 0 or j < 0:
+                    # Eliminate case list[-1], which is not
+                    # neighbour to this cell
+                    continue
+
+                try:
+
+                    neighbour = self.__board[i][j]
+
+                    if neighbour.isMine():
+                        self.gameOver()
+                        return
+
+                    if neighbour.isFlag():
+                        # Neighbour is flagged, so we don't want to check it
+                        continue
+
+                    # Check if neighbours neighbourcount is 0
+                    if neighbour.getCellNumber() == 0 and not neighbour.isRevealed():
+                        # Neighbour has no mines nearby, so we can reveal all its
+                        # neighbours too.
+
+                        neighbour.show()
+                        self.updateNeighbours(i, j)
+                    else:
+
+                        # Show and update neighbour
+                        neighbour.show()
+
+                    neighbour.update()
+
+                except IndexError:
+                    continue
+
+    def countNeighbours(self, x, y):
+        """ Counts how many neighbours cell has and """
+
+        cell = self.__board[x][y]
+        if cell.isRevealed():
+            return
+        minecount = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if i == 0 and j == 0:
+                    # We don't want to compare to ourself
+                    continue
+                if x+i < 0 or y + j < 0:
+                    # We dont want to check for top and botton cells together
+                    continue
+
+                try:
+                    neighbour = self.__board[x+i][y+j]
+                    if neighbour.isMine():
+                        minecount += 1
+                        continue
+
+                except IndexError:
+                    # We are on the edge of the board, no neighbours there...
+                    continue
+
+        cell.setCellNumber(minecount)
